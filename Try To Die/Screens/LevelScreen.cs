779d1e9005ab73503db6324714454f9b4466ca04
Devon Kennedy <kenneddevo@myvuw.ac.nz>
2018-11-15 11:43:37 +1300

    Add project files.

---
+++ Try To Die/Screens/LevelScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Try_To_Die.Utils;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Try_To_Die.Screens
+{
+    public class LevelScreen : GameScreen
+    {
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
